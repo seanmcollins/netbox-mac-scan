b0e4d26cf4359b88ff6c0f1e0d5de6fb7baf8971
Sean Collins <collsean@umich.edu>
2020-06-25 10:17:29 -0400

    Added url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='netbox-mac-scan',
     version='0.1',
     description='A Netbox plugin for MAC address scanning',
-    url='',
+    url='https://github.com/seanmcollins/netbox-mac-scan',
     author='Sean Collins',
     install_requires=[],
     packages=find_packages(),
